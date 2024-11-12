e60771cf451b980a3e74558ae4fe78269333149e
ArsKooz <71432162+ArsKooz@users.noreply.github.com>
2024-11-13 00:14:29 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Calculator
+{
+     static void Main()
+    {
+        float a = 0 , b = 0;
+
+        Console.WriteLine("Введите 2 числа");
+
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.WriteLine("a + b = "+ Convert.ToString(Addition(a,b)));
+        Console.WriteLine("a - b = " + Convert.ToString(Substraction(a,b)));
+        Console.WriteLine(("a * b = " + Convert.ToString(Multiplication(a,b))));
+        Console.WriteLine(("a / b = " + Convert.ToString(Division(a,b))));
+    }
+
+    static private float Addition(float a, float b) {return a + b;}
+    static private float Substraction(float a,float b) { return a - b; }
+    static private float Multiplication(float a,float b) {return a * b;}
+    static private float Division(float a,float b) {  return (a / b); }
+}
